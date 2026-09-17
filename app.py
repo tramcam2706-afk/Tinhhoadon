c2cda5cd789583f9d56620769affa6b89690a4b4
tramcam2706-afk <tramcam2706@gmail.com>
2026-09-17 16:00:00 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import pandas as pd
 from datetime import datetime
 st.set_page_config(page_title="Order Nhà Hàng", layout="wide")
  
-st.title("🍽️ Hệ thống Order Nhà Hàng - VŨ ĐỨC BÌNH")
+st.title("🍽️ Hệ thống Order Nhà Hàng - Bình Bé Bỏng")
  
 # ==========================
 # Khởi tạo dữ liệu
